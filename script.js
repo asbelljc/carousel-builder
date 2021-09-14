c4d43bf049f01be54b48c3f57514ef7b478ece09
asbelljc <asbelljc@gmail.com>
2021-09-14 13:58:51 -0400

    Made html background white again. Added navigation-dot logic to script and did a rough test for bugs. Seems to work well.

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
+const navDots = Array.from(document.querySelectorAll('.navigation-dots li'));
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 
-let counter = 1;
+let imageCounter = 1;
 
 carouselSlide.style.transform = 'translateX(-100%)';
+navDots[0].style.background = 'rgba(255, 255, 255, 0.8)';
 
 nextBtn.addEventListener('click', () => {
-  if (counter >= carouselImages.length - 1) return;
+  if (imageCounter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = 'transform 400ms';
-  counter++;
-  carouselSlide.style.transform = `translateX(-${100 * counter}%)`;
+  imageCounter++;
+  carouselSlide.style.transform = `translateX(-${100 * imageCounter}%)`;
 });
 
 prevBtn.addEventListener('click', () => {
-  if (counter <= 0) return;
+  if (imageCounter <= 0) return;
   carouselSlide.style.transition = 'transform 400ms';
-  counter--;
-  carouselSlide.style.transform = `translateX(-${100 * counter}%)`;
+  imageCounter--;
+  carouselSlide.style.transform = `translateX(-${100 * imageCounter}%)`;
 });
 
 carouselSlide.addEventListener('transitionend', () => {
-  if (carouselImages[counter].classList.contains('last-clone')) {
+  if (carouselImages[imageCounter].classList.contains('last-clone')) {
     carouselSlide.style.transition = 'none';
-    counter = carouselImages.length - 2;
-    carouselSlide.style.transform = `translateX(-${100 * counter}%)`;
+    imageCounter = carouselImages.length - 2;
+    carouselSlide.style.transform = `translateX(-${100 * imageCounter}%)`;
   }
-  if (carouselImages[counter].classList.contains('first-clone')) {
+
+  if (carouselImages[imageCounter].classList.contains('first-clone')) {
     carouselSlide.style.transition = 'none';
-    counter = carouselImages.length - counter;
-    carouselSlide.style.transform = `translateX(-${100 * counter}%)`;
+    imageCounter = carouselImages.length - imageCounter;
+    carouselSlide.style.transform = `translateX(-${100 * imageCounter}%)`;
   }
+
+  navDots.forEach((dot) => {
+    if (navDots.indexOf(dot) === imageCounter - 1) {
+      dot.style.background = 'rgba(255, 255, 255, 0.8)';
+    } else {
+      dot.style.background = 'transparent';
+    }
+  });
+});
+
+navDots.forEach((dot) => {
+  dot.addEventListener('click', (e) => {
+    carouselSlide.style.transition = 'transform 400ms';
+    imageCounter = 1 + navDots.indexOf(e.target);
+    carouselSlide.style.transform = `translateX(-${100 * imageCounter}%)`;
+  });
 });
